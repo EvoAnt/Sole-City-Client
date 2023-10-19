7ce7482d34d7b50e9a3abb3ad54a5925bdb2f2f1
Anthony Lara <Capone0824@gmail.com>
2023-10-19 18:23:02 -0400

    added pages, API services, basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import MyAccount from './pages/MyAccount'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import './App.css'
 
 function App() {
+
+  const getToken = () => {
+    return localStorage.getItem("authToken");
+  };
+
+  const LoggedIn = () => {
+    return getToken() ? <Outlet /> : <Navigate to="/login" />;
+  };
+
+  const NotLoggedIn = () => {
+    return !getToken() ? <Outlet /> : <Navigate to="/" />;
+  };
   
 
   return (
-    <div>Home Page</div>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/my-account" element={<MyAccount />} />
+
+
+
+
+
+      </Routes>
+
+
+
+
+
+
+
+    </div>
   )
 }
 
---
+++ src/services/authService.js
@@ -0,0 +1,19 @@
+import { API_URL } from "./API_URL";
+
+import axios from "axios";
+
+export const get = (route) => {
+  let token = localStorage.getItem("authToken");
+
+  return axios.get(API_URL + route, {
+    headers: { Authorization: `Bearer ${token}` },
+  })
+};
+
+export const post = (route, body) => {
+  let token = localStorage.getItem("authToken");
+
+  return axios.post(API_URL + route, body, {
+    headers: { Authorization: `Bearer ${token}` },
+  })
+};
