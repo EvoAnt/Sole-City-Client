b78757729a1aa76ca4b502f6eae3f5efda149bd3
Anthony Lara <Capone0824@gmail.com>
2023-10-26 16:17:20 -0400

    fixed carousel link bug

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -4,7 +4,7 @@ const CarouselComponent = () => {
   return (
     <Carousel fade={true}>
       <Carousel.Item>
-        <a href="http://localhost:5173/products/6532df5a8ad9ee1e772fe6b9">
+        <a href="https://solecity.netlify.app/products/653ac8976da9a86a43bb9619">
           <img
             className="d-block w-100"
             src="https://cdn.shopify.com/s/files/1/0852/0048/files/LHJordan1RedToeIG-1-1_1024x1024.jpg?v=1519078683"
@@ -14,7 +14,7 @@ const CarouselComponent = () => {
         </a>
       </Carousel.Item>
       <Carousel.Item>
-        <a href="http://localhost:5173/products/6532df5a8ad9ee1e772fe6b5">
+        <a href="http://solecity.netlify.app/products/6532df5a8ad9ee1e772fe6b5">
           <img
             className="d-block w-100"
             src="https://sneakernews.com/wp-content/uploads/2023/10/travis-scott-jordan-1-golf-release-date.jpg"
@@ -24,7 +24,7 @@ const CarouselComponent = () => {
         </a>
       </Carousel.Item>
       <Carousel.Item>
-        <a href="http://localhost:5173/products/6532df5a8ad9ee1e772fe6bd">
+        <a href="http://solecity.netlify.app/products/6532df5a8ad9ee1e772fe6bd">
           <img
             className="d-block w-100"
             src="https://sneakerbardetroit.com/wp-content/uploads/2020/10/Nike-SB-Dunk-low-freddy-kruger-sneaker-talk.jpg"
