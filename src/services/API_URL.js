ebd87bd406d07ecfd436e412229d4f2be1bbbc1b
Anthony Lara <Capone0824@gmail.com>
2026-04-24 18:55:21 -0400

    update API_URL to use Railway env variable

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:3000"
 
-export const API_URL = "https://sole-city.adaptable.app"
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000"
